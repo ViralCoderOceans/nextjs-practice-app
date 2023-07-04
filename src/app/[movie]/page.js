051da08df4821fd9b88f7b111126740502dc0be0
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-04 17:12:08 +0530

    fix: bug

--- src/app/[movie]/page.js
+++ src/app/[movie]/page.js
@@ -1,28 +1,55 @@
-import Image from 'next/image'
-import React from 'react'
+"use client"
 
-const Item = async ({ params }) => {
-    const { movie } = params
-    const imgPath = 'https://image.tmdb.org/t/p/original'
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
-    const res = await data.json()
-    return (
-        <div className="flex flex-col justify-between p-24 py-12">
-            <h1 className='text-2xl font-bold'>{res.title}</h1>
-            <hr className='my-2' />
-            <h2 className='text-sm'>Release Date : {res.release_date}</h2>
-            <h2 className='text-sm'>Runtime : {res.runtime} minutes </h2>
-            <h2 className='text-sm bg-green-600 w-min my-2 py-2 px-4 rounded-md'>{res.status}</h2>
-            <Image
-                className='my-12 w-full'
-                src={imgPath + res.backdrop_path}
-                width={2000}
-                height={2000}
-                priority
-            />
-            <p>{res.overview}</p>
-        </div>
-    )
+import axios from 'axios'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+
+const Item = ({ params }) => {
+  const [data, setData] = useState([])
+  const { movie } = params
+  const imgPath = 'https://image.tmdb.org/t/p/original'
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: `https://api.themoviedb.org/3/movie/${movie}`,
+      params: { language: 'en-US' },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTAwOTZmNjUwOTk3Y2EwYzU3OWJlYzEyNjAzOWYxYSIsInN1YiI6IjY0YTNjMDM2MTEzODZjMDExYzNiMmExNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WvfGV_JD6bF4c0oZDwZdRXr6jKHlRtDAyTsTrBwHMM0'
+      }
+    };
+    async function getData() {
+      await axios
+        .request(options)
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+    getData()
+  }, [])
+  // const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
+  // const res = await data.json()
+  return (
+    <div className="flex flex-col justify-between p-24 py-12">
+      <h1 className='text-2xl font-bold'>{data.title}</h1>
+      <hr className='my-2' />
+      <h2 className='text-sm'>Release Date : {data.release_date}</h2>
+      <h2 className='text-sm'>Runtime : {data.runtime} minutes </h2>
+      <h2 className='text-sm bg-green-600 w-min my-2 py-2 px-4 rounded-md'>{data.status}</h2>
+      <Image
+        className='my-12 w-full'
+        src={imgPath + data.backdrop_path}
+        width={2000}
+        height={2000}
+        alt={data.title}
+        priority
+      />
+      <p>{data.overview}</p>
+    </div>
+  )
 }
 
 export default Item
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,35 @@
-import Movie from './Movie'
+"use client"
 
-export default async function Home() {
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTAwOTZmNjUwOTk3Y2EwYzU3OWJlYzEyNjAzOWYxYSIsInN1YiI6IjY0YTNjMDM2MTEzODZjMDExYzNiMmExNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WvfGV_JD6bF4c0oZDwZdRXr6jKHlRtDAyTsTrBwHMM0'
-  //   }
-  // };
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
-  const res = await data.json()
+import { useEffect, useState } from 'react'
+import Movie from './Movie'
+import axios from 'axios'
+
+export default function Home() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/account/20104985/favorite/movies',
+      params: { language: 'en-US', page: '1', sort_by: 'created_at.asc' },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTAwOTZmNjUwOTk3Y2EwYzU3OWJlYzEyNjAzOWYxYSIsInN1YiI6IjY0YTNjMDM2MTEzODZjMDExYzNiMmExNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WvfGV_JD6bF4c0oZDwZdRXr6jKHlRtDAyTsTrBwHMM0'
+      }
+    }
+    async function getData() {
+      await axios
+        .request(options)
+        .then((response) => {
+          setData(response.data.results)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+    getData()
+  }, [])
+  // const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  // const res = await data.json()
   return (
     <main className="flex flex-col justify-between p-24 py-12">
       <div>
@@ -17,7 +37,7 @@ export default async function Home() {
         <hr className='my-2' />
         <div className='grid grid-cols-fluid'>
           {
-            res?.results?.map((elm) => (
+            data.map((elm) => (
               <Movie
                 key={elm.id}
                 id={elm.id}
