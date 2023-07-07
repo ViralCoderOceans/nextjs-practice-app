6936b35e9c3e60453973b025095ad27cb75d6cd5
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-07 13:04:27 +0530

    update: add useCallback

--- src/hooks/useGetMovieDetails.js
+++ src/hooks/useGetMovieDetails.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 import { useParams } from "next/navigation";
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 const useGetMovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState([])
   const params = useParams();
   const movie = params.movie;
-  const options = {
-    method: 'GET',
-    url: `https://api.themoviedb.org/3/movie/${movie}`,
-    params: { language: 'en-US' },
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
-    }
-  };
 
-  const getMovieDetails = async () => {
+  const getMovieDetails = useCallback(async () => {
+    const options = {
+      method: 'GET',
+      url: `https://api.themoviedb.org/3/movie/${movie}`,
+      params: { language: 'en-US' },
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
+      }
+    };
     await axios.request(options).then((response) => {
       if (response.status === 200) {
         setMovieDetails(response.data)
@@ -24,7 +24,7 @@ const useGetMovieDetails = () => {
         console.error(response)
       }
     })
-  }
+  }, [])
 
 
   return { getMovieDetails, movieDetails }
