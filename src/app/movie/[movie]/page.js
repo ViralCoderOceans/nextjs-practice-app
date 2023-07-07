9895ac8e6cab789d40df9ee29cd05562f66ce32c
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-07 13:06:06 +0530

    add: genres

--- src/app/movie/[movie]/page.js
+++ src/app/movie/[movie]/page.js
@@ -3,20 +3,22 @@
 import useGetMovieDetails from '@/hooks/useGetMovieDetails'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import placeHolder from 'src/app/placeholder.png'
 
 const Item = () => {
-  // const [imgPath, setImgPath] = useState('')
   const { getMovieDetails, movieDetails } = useGetMovieDetails()
+  const [genres, setGenres] = useState([12, 212, 12, 1212])
   useEffect(() => {
     getMovieDetails()
   }, [])
-  // useEffect(() => {
-  //   setImgPath(`https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`)
-  // }, [movieDetails])
-  // const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
-  // const res = await data.json()
+  useEffect(() => {
+    if (movieDetails.length !== 0) {
+      const refArr = []
+      movieDetails.genres.map((elm) => refArr.push(elm.name))
+      setGenres([...refArr])
+    }
+  }, [movieDetails])
   return (
     <>
       {
@@ -28,32 +30,30 @@ const Item = () => {
               <div className='my-2 mt-1 bg-zinc-900 h-px' />
             </div>
             <div className='animate-pulse'>
-              <div className='bg-zinc-900 h-5 w-[200px]' />
-              <div className='bg-zinc-900 h-5 w-[150px]' />
+              <div className='bg-zinc-900 h-5 w-full' />
             </div>
             <div className='animate-pulse flex gap-4 my-2'>
               <div className='bg-zinc-900 h-10 w-[145px]' />
               <div className='bg-zinc-900 h-10 w-[110px]' />
             </div>
             <div className='animate-pulse my-6'>
-              <div className='bg-zinc-900 h-[610px] w-full' />
+              <div className='bg-zinc-900 h-[300px] md:h-[500px] lg:h-[675px] w-full' />
             </div>
             <div className='animate-pulse'>
-              <div className='bg-zinc-900 h-6 w-full' />
-              <div className='bg-zinc-900 h-6 w-full' />
-              <div className='bg-zinc-900 h-6 w-5/12' />
+              <div className='bg-zinc-900 h-4 w-full mb-1' />
+              <div className='bg-zinc-900 h-4 w-full mb-1' />
+              <div className='bg-zinc-900 h-4 w-5/12' />
             </div>
           </>
           : <>
             <hr className='my-2 mb-1' />
-            <h1 className='text-2xl font-bold py-2 bg-white text-black ps-4'>{movieDetails.title}</h1>
+            <h1 className='text-lg md:text-xl lg:text-2xl font-bold py-2 bg-white text-black ps-4'>{movieDetails.title}</h1>
             <hr className='my-2 mt-1' />
-            <h2 className='text-sm'>Release Date : {movieDetails.release_date}</h2>
-            <h2 className='text-sm'>Runtime : {movieDetails.runtime} minutes </h2>
-            <div className='flex flex-row items-stretch'>
-              <h2 className='text-sm bg-green-600 my-2 py-2 px-4 flex items-center'>Status: {movieDetails.status || 'Fetching...'}</h2>
-              <Link href={`${movieDetails?.homepage}`}>
-                <button className='text-sm font-semibold mx-4 my-2 py-2 px-4 border-2 border-blue-600 bg-blue-600 hover:bg-zinc-900 transition-all'>
+            <h2 className='text-base'>&bull; {movieDetails.release_date} &bull; {genres.join(', ')} &bull; {movieDetails.runtime} minutes </h2>
+            <div className='flex flex-row items-stretch my-2'>
+              <h2 className='text-sm bg-green-600 py-2 px-4 flex items-center mr-4'>Status: {movieDetails.status || 'Fetching...'}</h2>
+              <Link href={`${movieDetails?.homepage}`} target='_blank'>
+                <button className='text-sm font-semibold py-2 px-4 border-2 border-blue-600 bg-blue-600 hover:bg-zinc-900 transition-all'>
                   Watch Now
                 </button>
               </Link>
