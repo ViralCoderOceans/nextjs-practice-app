bbcb9c0ac581a240f28566edbea1fd463bdff8da
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-10 17:41:17 +0530

    add: const-file

---
+++ constants/constants.js
@@ -0,0 +1,26 @@
+export const allCategories = [
+    {
+        id: 1,
+        path: 'now_playing',
+        title: 'Now Playing',
+        api_url: 'movie/now_playing'
+    },
+    {
+        id: 2,
+        path: 'popular',
+        title: 'Popular',
+        api_url: 'movie/popular'
+    },
+    {
+        id: 3,
+        path: 'top_rated',
+        title: 'Top Rated',
+        api_url: 'movie/top_rated'
+    },
+    {
+        id: 4,
+        path: 'upcoming',
+        title: 'Upcoming',
+        api_url: 'movie/upcoming'
+    }
+]
