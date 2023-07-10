39b5c518ed86c82951159d29baafe269c5527097
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-10 19:37:47 +0530

    update: hide-navbar-in-login-page

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,8 @@ export default function RootLayout({ children }) {
     movieId: ''
   })
   const [isMinimize, setIsMinimize] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  console.log('isLoggedIn: ', isLoggedIn)
   const path = usePathname()
   return (
     <html lang="en">
@@ -27,31 +29,33 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={`${inter.className} flex flex-col h-screen relative overflow-hidden`}>
-        <nav className='w-full z-[99] flex flex-row justify-between px-6 lg:px-24 bg-zinc-950 hover:bg-zinc-900 transition-all'>
-          <Link href={`/`}>
-            <h1 className='text-lg md:text-xl lg:text-2xl px-1 lg:px-3 py-3 lg:py-6 font-semibold bg-white text-black transition-all'>
-              Movie_app
-            </h1>
-          </Link>
-          <ul className='flex flex-row items-center'>
-            <li className='h-full'>
-              <Link href={`/`} className={`text-sm lg:text-base flex items-center px-1 lg:px-4 mr-2 h-full hover:bg-white hover:text-black transition-all ${path == '/' ? 'bg-white text-black' : ''}`}>
-                Home
-              </Link>
-            </li>
-            <li className='h-full'>
-              <Link href={`/about`} className={`text-sm lg:text-base flex items-center px-1 lg:px-4 mr-2 h-full hover:bg-white hover:text-black transition-all ${path == '/about' ? 'bg-white text-black' : ''}`}>
-                About
-              </Link>
-            </li>
-            <li className='h-full'>
-              <Link href={`/profile`} className={`text-sm lg:text-base flex items-center px-1 lg:px-4 h-full hover:bg-white hover:text-black transition-all ${path == '/profile' ? 'bg-white text-black' : ''}`}>
-                Profile
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <piPContext.Provider value={{ pictureInPicture, setPictureInPicture, isMinimize, setIsMinimize }}>
+        {isLoggedIn &&
+          <nav className='w-full z-[99] flex flex-row justify-between px-6 lg:px-24 bg-zinc-950 hover:bg-zinc-900 transition-all'>
+            <Link href={`/home`}>
+              <h1 className='text-lg md:text-xl lg:text-2xl px-1 lg:px-3 py-3 lg:py-6 font-semibold bg-white text-black transition-all'>
+                Movie_app
+              </h1>
+            </Link>
+            <ul className='flex flex-row items-center'>
+              <li className='h-full'>
+                <Link href={`/home`} className={`text-sm lg:text-base flex items-center px-1 lg:px-4 mr-2 h-full hover:bg-white hover:text-black transition-all ${path == '/home' ? 'bg-white text-black' : ''}`}>
+                  Home
+                </Link>
+              </li>
+              <li className='h-full'>
+                <Link href={`/about`} className={`text-sm lg:text-base flex items-center px-1 lg:px-4 mr-2 h-full hover:bg-white hover:text-black transition-all ${path == '/about' ? 'bg-white text-black' : ''}`}>
+                  About
+                </Link>
+              </li>
+              <li className='h-full'>
+                <Link href={`/profile`} className={`text-sm lg:text-base flex items-center px-1 lg:px-4 h-full hover:bg-white hover:text-black transition-all ${path == '/profile' ? 'bg-white text-black' : ''}`}>
+                  Profile
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        }
+        <piPContext.Provider value={{ pictureInPicture, setPictureInPicture, isMinimize, setIsMinimize, isLoggedIn, setIsLoggedIn }}>
           <div className="flex flex-col justify-between p-4 lg:px-24 lg:py-12 overflow-y-auto overflow-x-hidden transition-all">
             {children}
             <div className='absolute bottom-0 right-0 '>
