2acd4ebc6c6641796094ddcab4a15bd08ff2285a
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-13 19:02:36 +0530

    remove: unnecessary comments

--- src/app/Movie.jsx
+++ src/app/Movie.jsx
@@ -4,11 +4,8 @@ import Image from 'next/image'
 import React, { useContext } from 'react'
 import placeHolder from '../app/placeholder_ver.png'
 import { piPContext } from './layout'
-import { useRouter } from 'next/navigation'
 
 const Movie = ({ title, id, poster_path, release_date, backdrop_path }) => {
-  // const imgPath = `https://image.tmdb.org/t/p/original${poster_path}`
-  const router = useRouter()
   const { pictureInPicture, setPictureInPicture } = useContext(piPContext);
   const handleOnClick = () => {
     if (pictureInPicture.isPIP) {
@@ -18,10 +15,7 @@ const Movie = ({ title, id, poster_path, release_date, backdrop_path }) => {
         imgPath: backdrop_path !== null ? `https://image.tmdb.org/t/p/original${backdrop_path}` : null,
         movieId: id
       })
-    } 
-    // else {
-    //   router.push(`/movie/${id}`)
-    // }
+    }
   }
   return (
     <div onClick={handleOnClick} className={`p-4 h-full flex flex-col justify-between text-center hover:bg-zinc-900 transition-all cursor-pointer`}>
@@ -38,7 +32,6 @@ const Movie = ({ title, id, poster_path, release_date, backdrop_path }) => {
           className='w-full transition-all opacity-0 duration-[1s] animate-pulse bg-zinc-800 h-[360px]'
           onLoadingComplete={(image) => {
             image.classList.remove('opacity-0')
-            // image.classList.remove('blur-xl')
             image.classList.remove('animate-pulse')
             image.classList.remove('bg-zinc-800')
             image.classList.remove('h-[360px]')
