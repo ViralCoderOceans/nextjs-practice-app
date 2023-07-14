34685ef3bb4bee45ef30baf68648e563169a82d7
ViralCoderOceans <viral.coderoceans@gmail.com>
2023-07-14 17:19:47 +0530

    remove: unnecessary part

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useState } from 'react'
 import PictureInPicture from '@/components/PictureInPicture'
 import { usePathname } from 'next/navigation'
 
